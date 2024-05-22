3727b1e63886715f796e8417ac232b0b76d3b605
DanilaTravkov <ygnbeats@gmail.com>
2024-05-22 13:50:42 +0200

    Added fields to Notification

--- WPFTutorial/Model/Notification.cs
+++ WPFTutorial/Model/Notification.cs
@@ -8,5 +8,14 @@ namespace WPFTutorial.Model
 {
     public class Notification
     {
+        public Teacher Sender { get; set; }
+        public Student Reciever { get; set; }
+        public bool IsStudentAccepted {  get; set; }
+        public string? MessageFromTeacher { get; set; }
+        public bool IsCancelledByStudent {  get; set; }
+        public string? MessageFromStudent { get; set; }
+        public bool StudentProbableCause {  get; set; }
+
+        public Notification() { }
     }
 }
